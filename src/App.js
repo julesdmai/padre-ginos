4f3372f9791cd0847dd98fcf7f1d46727994bf4b
julesdmai <julesdmai@gmail.com>
2025-02-28 00:18:21 -0800

    Added gitignore

--- src/App.js
+++ src/App.js
@@ -1,39 +1,35 @@
 const Pizza = (props) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, props.name),
-        React.createElement("p", {}, props.description),
-    ]);
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
+};
 
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, "Padre Gino's"),
-            React.createElement(Pizza, {
-                name: "The Pepperoni",
-                description: "Mozzarella Cheese, Pepperoni"
-            }),
-            React.createElement(Pizza, {
-                name: "The Hawaiian",
-                description: "Pineapples"
-            }),
-            React.createElement(Pizza, {
-                name: "The New York",
-                description: "I'm walkin' ova 'ere"
-            }),
-            React.createElement(Pizza, {
-                name: "The Chicago",
-                description: "Deep dish"
-            }),
-            React.createElement(Pizza, {
-                name: "The Canadian",
-                description: "Maple syrup"
-            }),
-        ]
-    )
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Padre Gino's"),
+    React.createElement(Pizza, {
+      name: "The Pepperoni",
+      description: "Mozzarella Cheese, Pepperoni",
+    }),
+    React.createElement(Pizza, {
+      name: "The Hawaiian",
+      description: "Pineapples",
+    }),
+    React.createElement(Pizza, {
+      name: "The New York",
+      description: "I'm walkin' ova 'ere",
+    }),
+    React.createElement(Pizza, {
+      name: "The Chicago",
+      description: "Deep dish",
+    }),
+    React.createElement(Pizza, {
+      name: "The Canadian",
+      description: "Maple syrup",
+    }),
+  ]);
+};
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
