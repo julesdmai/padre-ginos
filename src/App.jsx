51d97b5e546822a24013e1d493dc9ce72ecbeb1b
julesdmai <julesdmai@gmail.com>
2025-02-28 20:43:30 -0800

    Added Order.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import Pizza from "./Pizza";
+import Order from "./Order";
 
 const App = () => {
   return (
     <div>
       <h1>Padre Gino's - Order Now</h1>
-      <Pizza
-        name="Pepperoni"
-        description="pep, cheese, n stuff"
-        image={"/public/pizzas/pepperoni.webp"}
-      />
-      <Pizza
-        name="Hawaiian"
-        description="ham, pineapple, n stuff"
-        image={"/public/pizzas/hawaiian.webp"}
-      />
-      <Pizza
-        name="Americano"
-        description="hot dogs, french fries, n stuff"
-        image={"/public/pizzas/big_meat.webp"}
-      />
+      <Order />
     </div>
   );
 };
