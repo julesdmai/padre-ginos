5318a9f256a0d6232ca820cc651779217bb22246
julesdmai <julesdmai@gmail.com>
2025-03-01 01:44:58 -0800

    Functioning modal

--- src/api/getPastOrder.js
+++ src/api/getPastOrder.js
@@ -1,6 +1,6 @@
 export default async function getPastOrder(orderNo) {
+  await new Promise((resolve) => setTimeout(resolve, 5000)); // 5s delay
   const response = await fetch(`/api/past-order/${orderNo}`);
   const data = await response.json();
   return data;
 }
- 
