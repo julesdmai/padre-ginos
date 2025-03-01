12e290d4c9ded74b584ae426c83a9509f11c707b
julesdmai <julesdmai@gmail.com>
2025-02-28 21:37:27 -0800

    Debugging not rendering option - figured it out needs implicit return

--- src/Order.jsx
+++ src/Order.jsx
@@ -1,12 +1,43 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Pizza from "./Pizza";
 
-export default function Order() {
-  //   const pizzaType = "pepperoni";
-  //   const pizzaSize = "M";
+const intl = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
 
+export default function Order() {
+  const [pizzaTypes, setPizzaTypes] = useState([]);
   const [pizzaType, setPizzaType] = useState("pepperoni");
   const [pizzaSize, setPizzaSize] = useState("M");
+  const [loading, setLoading] = useState(true);
+
+  let price, selectedPizza;
+  if (!loading) {
+    selectedPizza = pizzaTypes.find((pizza) => pizzaType === pizza.id);
+  }
+
+  async function fetchPizzaTypes() {
+    const pizzaRes = await fetch("/api/pizzas");
+    const pizzaJson = await pizzaRes.json();
+    setPizzaTypes(pizzaJson);
+    };
+
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchPizzaTypes();
+  }, []);
+
+  //   Debugging
+  useEffect(() => {
+    console.log("Updated pizzaTypes: ", pizzaTypes);
+    console.log(
+      "Mapped pizzaTypes: ",
+      pizzaTypes.map((p) => ({ id: p.id, name: p.name }))
+    );
+  }, [pizzaTypes]);
 
   return (
     <div className="order">
@@ -20,9 +51,11 @@ export default function Order() {
               name="pizza-type"
               value={pizzaType}
             >
-              <option value="pepperoni"> The Pepperoni Pizza</option>
-              <option value="hawaiian"> The Hawaiian Pizza</option>
-              <option value="big_meat"> The Big Meat Pizza</option>
+              {pizzaTypes.map((pizza) => (
+                  <option key={pizza.id} value={pizza.id}>
+                    {pizza.name}
+                  </option>
+              ))}
             </select>
           </div>
           <div>
