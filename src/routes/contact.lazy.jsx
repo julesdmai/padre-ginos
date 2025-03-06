664d12e9d9019532bd602abacf84142fb22bc2e4
julesdmai <julesdmai@gmail.com>
2025-03-05 22:41:44 -0800

    useFormStatus ContactInput

--- src/routes/contact.lazy.jsx
+++ src/routes/contact.lazy.jsx
@@ -1,3 +1,4 @@
+import { useFormStatus } from 'react-dom';
 import { createLazyFileRoute } from "@tanstack/react-router";
 import { useMutation } from "@tanstack/react-query";
 import postContact from "../api/postContact";
@@ -25,12 +26,24 @@ function ContactRoute() {
         <h3>Submitted</h3>
       ) : (
         <form action={mutation.mutate}>
-          <input name="name" placeholder="Name" type="text" />
-          <input name="email" placeholder="Email" type="email" />
+          <ContactInput name="name" placeholder="Name" type="text" />
+          <ContactInput name="email" placeholder="Email" type="email" />
           <textarea name="message" placeholder="Message"></textarea>
           <button>Submit</button>
         </form>
       )}
     </div>
+  );
+}
+
+function ContactInput (props) {
+  const { pending } = useFormStatus();
+  return (
+    <input
+    disabled={pending}
+    name={props.name}
+    type={props.type}
+    placeholder={props.placeholder}
+    />
   );
 }
