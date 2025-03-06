ef694a1a8e64e214c674962af0d2bc5fdc8c51a4
julesdmai <julesdmai@gmail.com>
2025-03-05 21:13:27 -0800

    Using renderHook

--- src/__tests__/usePizzaOfTheDay.test.jsx
+++ src/__tests__/usePizzaOfTheDay.test.jsx
@@ -1,5 +1,5 @@
 import { expect, test, vi } from "vitest";
-import { render } from "@testing-library/react";
+import { renderHook } from "@testing-library/react";
 import createFetchMock from "vitest-fetch-mock";
 import { usePizzaOfTheDay } from "../usePizzaOfTheDay";
 
@@ -15,21 +15,8 @@ const testPizza = {
     size: { S: 12.25, M: 16.25, L: 20.25 },
 }
 
-function getPizzaOfTheDay() {
-    let pizza;
-
-    function TestComponent() {
-        pizza = usePizzaOfTheDay();
-        return null;
-    }
-
-    render(<TestComponent/>);
-
-    return pizza;
-}
-
 test("gives null when first called", async () => {
     fetch.mockResponseOnce(JSON.stringify(testPizza));
-    const pizza = getPizzaOfTheDay();
-    expect(pizza).toBeNull();
+    const { result } = renderHook(() => usePizzaOfTheDay());
+    expect(result.current).toBeNull();
 })
