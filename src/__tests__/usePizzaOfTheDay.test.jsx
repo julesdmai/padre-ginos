74ab38508eec42589a66d3f06579939bbd87707a
julesdmai <julesdmai@gmail.com>
2025-03-05 21:27:19 -0800

    Tested usePizzaOfTheDay custom hook

--- src/__tests__/usePizzaOfTheDay.test.jsx
+++ src/__tests__/usePizzaOfTheDay.test.jsx
@@ -1,5 +1,5 @@
 import { expect, test, vi } from "vitest";
-import { renderHook } from "@testing-library/react";
+import { renderHook, waitFor } from "@testing-library/react";
 import createFetchMock from "vitest-fetch-mock";
 import { usePizzaOfTheDay } from "../usePizzaOfTheDay";
 
@@ -13,10 +13,19 @@ const testPizza = {
     description: "Pizza from Calabria",
     imagee: "/public/pizzas/calabrese.webp",
     size: { S: 12.25, M: 16.25, L: 20.25 },
-}
+};
 
 test("gives null when first called", async () => {
     fetch.mockResponseOnce(JSON.stringify(testPizza));
     const { result } = renderHook(() => usePizzaOfTheDay());
     expect(result.current).toBeNull();
-})
+});
+
+test("to call the API and receive the response of the pizza of the day", async () => {
+    fetch.mockResponseOnce(JSON.stringify(testPizza));
+    const { result } = renderHook(() => usePizzaOfTheDay());
+    await waitFor(() => {
+        expect(result.current).toEqual(testPizza);
+    });
+    expect(fetchMocker).toBeCalledWith("/api/pizza-of-the-day");
+});
